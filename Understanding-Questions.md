a3cb8308bb37d691e7c157e51dbee58a778ef89c
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2021-11-01 16:15:08 -0500

    Added number functionality, operations

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -2,6 +2,9 @@
 1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
 * The user presses the 1 button.
 * 
-...
+
+The action creator gets called in the action file, and this removes any hiccups with using strings and converts them. 
+
+Reducer gets called in the reducer file and then goes to the ADD_ONE case, spreading the state and adding one.
 
 * TotalDisplay shows the total plus 1.
--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-
+import { useReducer } from 'react';
+import { addOne, applyNumber, changeOperation} from './actions';
 import './App.css';
 
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
+import reducer, { initialState } from './reducers';
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const handleAddOne = () => {
+    dispatch(addOne());
+  }
+
+  function handleApplyNumber(num) {
+    dispatch(applyNumber(num));
+  }
+
+  function handleChangeOperation(operator) {
+    dispatch(changeOperation(operator));
+  }
 
   return (
     <div className="App">
@@ -17,10 +32,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
@@ -30,25 +45,25 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={1} onClick={() => handleApplyNumber(1)}/>
+              <CalcButton value={2} onClick={() => handleApplyNumber(2)}/>
+              <CalcButton value={3} onClick={() => handleApplyNumber(3)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} onClick={() => handleApplyNumber(4)}/>
+              <CalcButton value={5} onClick={() => handleApplyNumber(5)}/>
+              <CalcButton value={6} onClick={() => handleApplyNumber(6)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick={() => handleApplyNumber(7)}/>
+              <CalcButton value={8} onClick={() => handleApplyNumber(8)}/>
+              <CalcButton value={9} onClick={() => handleApplyNumber(9)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
+              <CalcButton value={"+"} onClick={() => handleChangeOperation("+")}/>
               <CalcButton value={"*"}/>
               <CalcButton value={"-"}/>
             </div>
